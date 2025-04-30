8ffdbf915af83962293d53b6d9b66bb095a82a63
Timur <ab.ti.wo@gmail.com>
2025-04-30 20:42:55 +0500

    Адаптация шаблона первые шаги , добавлено html, moduls, gulpfire, package.json ,index.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Привет от Vue!</h1>
+    <p>Это минимальное Vue-приложение, интегрированное в наш Django-проект.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+  padding: 20px;
+}
+</style>
+
