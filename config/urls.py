cca349ae98d54df5f9a882cd3a3a62238674bd1c
Timur <ab.ti.wo@gmail.com>
2025-08-13 13:05:24 +0500

    обновлен urls.py в config

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,12 @@ from django.urls import path, include
 from django.views.static import serve
 from django.conf import settings
 
-urlpatterns =+ [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
+]
+
+urlpatterns += [
     path("robots.txt", serve, {'path': 'robots.txt', 'document_root': settings.STATIC_ROOT}),
     path("ads.txt", serve, {'path': 'ads.txt', 'document_root': settings.STATIC_ROOT}),
     path("security.txt", serve, {'path': 'security.txt', 'document_root': settings.STATIC_ROOT}),
